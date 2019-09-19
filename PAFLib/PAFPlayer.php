eb6b21a1151378c9b6afca018283ccaafd446889
Simonsator <00pflaume@gmail.com>
2019-09-19 15:13:39 +0200

    Using tabs instead of spaces

--- PAFLib/PAFPlayer.php
+++ PAFLib/PAFPlayer.php
@@ -8,45 +8,43 @@
 namespace PartyAndFriends\Lib\PAFPlayer;
 require_once('PAFPlayerManager.php');
 
-use PartyAndFriends\Lib\PAFPlayer\PAFPlayerManager;
-
 class PAFPlayer
 {
-    private $uniqueID;
-    private $name;
-    private $id;
+	private $uniqueID;
+	private $name;
+	private $id;
 
-    function __construct($pUUID, $pName, $pID)
-    {
-        $this->uniqueID = $pUUID;
-        $this->name = $pName;
-        $this->id = $pID;
-    }
+	function __construct($pUUID, $pName, $pID)
+	{
+		$this->uniqueID = $pUUID;
+		$this->name = $pName;
+		$this->id = $pID;
+	}
 
-    function getName()
-    {
-        return $this->name;
-    }
+	function getName()
+	{
+		return $this->name;
+	}
 
-    function getUniqueID()
-    {
-        return $this->uniqueID;
-    }
+	function getUniqueID()
+	{
+		return $this->uniqueID;
+	}
 
-    function getID()
-    {
-        return $this->id;
-    }
+	function getID()
+	{
+		return $this->id;
+	}
 
-    function getFriends()
-    {
-        $stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("SELECT player_id, player_uuid, player_name FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "_players WHERE player_id IN(SELECT friend1_id FROM fr_friend_assignment WHERE friend2_id='" . $this->id . "') OR player_id IN(SELECT friend2_id FROM fr_friend_assignment WHERE friend1_id='" . $this->id . "') LIMIT 1");
-        $stmt->execute();
-        $i = 0;
-        foreach ($stmt as $row) {
-            $friends[$i] = new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
-            $i++;
-        }
-        return $friends;
-    }
+	function getFriends()
+	{
+		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("SELECT player_id, player_uuid, player_name FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "_players WHERE player_id IN(SELECT friend1_id FROM fr_friend_assignment WHERE friend2_id='" . $this->id . "') OR player_id IN(SELECT friend2_id FROM fr_friend_assignment WHERE friend1_id='" . $this->id . "') LIMIT 1");
+		$stmt->execute();
+		$i = 0;
+		foreach ($stmt as $row) {
+			$friends[$i] = new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
+			$i++;
+		}
+		return $friends;
+	}
 }
--- PAFLib/PAFPlayerManager.php
+++ PAFLib/PAFPlayerManager.php
@@ -14,69 +14,69 @@ require_once('PAFPlayer.php');
  */
 class PAFPlayerManager
 {
-    private $connection;
-    private $tablePrefix;
-    private static $instance;
+	private static $instance;
+	private $connection;
+	private $tablePrefix;
 
-    function __construct($pPod, $tablePrefix)
-    {
-        self::$instance = $this;
-        $this->connection = $pPod;
-        $this->tablePrefix = $tablePrefix;
-    }
+	function __construct($pPod, $tablePrefix)
+	{
+		self::$instance = $this;
+		$this->connection = $pPod;
+		$this->tablePrefix = $tablePrefix;
+	}
 
-    public function getPlayerByUUID($pUUID)
-    {
-        $stmt = $this->connection->prepare("SELECT player_id, player_uuid, player_name FROM fr_players WHERE player_uuid=:uuid LIMIT 1");
-        $stmt->bindParam(':uuid', $pUUID);
-        $stmt->execute();
-        if ($stmt->rowCount() == 0) {
-            return NULL;
-        }
-        $row = $stmt->fetch();
-        return new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
-    }
+	public function getPlayerByUUID($pUUID)
+	{
+		$stmt = $this->connection->prepare("SELECT player_id, player_uuid, player_name FROM fr_players WHERE player_uuid=:uuid LIMIT 1");
+		$stmt->bindParam(':uuid', $pUUID);
+		$stmt->execute();
+		if ($stmt->rowCount() == 0) {
+			return NULL;
+		}
+		$row = $stmt->fetch();
+		return new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
+	}
 
-    public function getPlayerByID($pID)
-    {
-        $stmt = $this->connection->prepare("SELECT player_id, player_uuid, player_name FROM fr_players WHERE player_id=:id LIMIT 1");
-        $stmt->bindParam(':id', $pID);
-        $stmt->execute();
-        if ($stmt->rowCount() == 0) {
-            return NULL;
-        }
-        $row = $stmt->fetch();
-        return new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
-    }
+	public function getPlayerByID($pID)
+	{
+		$stmt = $this->connection->prepare("SELECT player_id, player_uuid, player_name FROM fr_players WHERE player_id=:id LIMIT 1");
+		$stmt->bindParam(':id', $pID);
+		$stmt->execute();
+		if ($stmt->rowCount() == 0) {
+			return NULL;
+		}
+		$row = $stmt->fetch();
+		return new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
+	}
 
-    public function getConnection()
-    {
-        return $this->connection;
-    }
+	public function getConnection()
+	{
+		return $this->connection;
+	}
 
-    public static function getInstance()
-    {
-        return self::$instance;
-    }
+	public function getPlayerByName($pPlayerName)
+	{
+		$stmt = $this->connection->prepare("SELECT player_id, player_uuid, player_name FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "players WHERE player_name=:name LIMIT 1");
+		$stmt->bindParam(':name', $pPlayerName);
+		$stmt->execute();
+		if ($stmt->rowCount() == 0) {
+			return NULL;
+		}
+		$row = $stmt->fetch();
+		return new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
+	}
 
-    public function getPlayerByName($pPlayerName)
-    {
-        $stmt = $this->connection->prepare("SELECT player_id, player_uuid, player_name FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "players WHERE player_name=:name LIMIT 1");
-        $stmt->bindParam(':name', $pPlayerName);
-        $stmt->execute();
-        if ($stmt->rowCount() == 0) {
-            return NULL;
-        }
-        $row = $stmt->fetch();
-        return new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
-    }
+	/**
+	 * @return String
+	 */
+	public function getTablePrefix()
+	{
+		return $this->tablePrefix;
+	}
 
-    /**
-     * @return String
-     */
-    public function getTablePrefix()
-    {
-        return $this->tablePrefix;
-    }
+	public static function getInstance()
+	{
+		return self::$instance;
+	}
 
 }
