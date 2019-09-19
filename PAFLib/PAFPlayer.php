1230ab92f7b6cc46bff879da04f0379fec9ce719
Simonsator <00pflaume@gmail.com>
2019-09-19 15:17:26 +0200

    Made functions public

--- PAFLib/PAFPlayer.php
+++ PAFLib/PAFPlayer.php
@@ -19,19 +19,19 @@ class PAFPlayer {
 		$this->id = $pID;
 	}
 
-	function getName() {
+	public function getName() {
 		return $this->name;
 	}
 
-	function getUniqueID() {
+	public function getUniqueID() {
 		return $this->uniqueID;
 	}
 
-	function getID() {
+	public function getID() {
 		return $this->id;
 	}
 
-	function getFriends() {
+	public function getFriends() {
 		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("SELECT player_id, player_uuid, player_name FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "_players WHERE player_id IN(SELECT friend1_id FROM fr_friend_assignment WHERE friend2_id='" . $this->id . "') OR player_id IN(SELECT friend2_id FROM fr_friend_assignment WHERE friend1_id='" . $this->id . "') LIMIT 1");
 		$stmt->execute();
 		$i = 0;
