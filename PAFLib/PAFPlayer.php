92cc276db3e0ff49961f9a462254459346358ec5
Ryan <46093784+Chezzer-ub@users.noreply.github.com>
2021-06-03 20:35:45 +1200

    Add setting change ability
    Hope this is good enough haha, tested and working on my website.

--- PAFLib/PAFPlayer.php
+++ PAFLib/PAFPlayer.php
@@ -9,83 +9,126 @@ namespace PartyAndFriends\Lib\PAFPlayer;
 require_once('PAFPlayerManager.php');
 
 class PAFPlayer {
-	private string $uniqueID;
-	private string $name;
-	private int $id;
+	private $uniqueID;
+	private $name;
+	private $id;
+	private $settingMap;
 
-	function __construct(string $pUUID, string $pName, int $pID) {
+	function __construct($pUUID, $pName, $pID) {
 		$this->uniqueID = $pUUID;
 		$this->name = $pName;
 		$this->id = $pID;
+
+		$this->settingMap = array(
+			"hide" => 6,
+			"notifications" => 101,
+			"online" => 3,
+			"receive" => 2,
+			"invite" => 1,
+			"requests" => 0
+		);
 	}
 
-	public function getName(): string {
+	public function getName() {
 		return $this->name;
 	}
 
-	public function getUniqueID(): string {
+	public function getUniqueID() {
 		return $this->uniqueID;
 	}
 
-	public function getFriends(): array {
-		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("SELECT player_id, player_uuid, player_name FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "players WHERE player_id IN(SELECT friend1_id FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "friend_assignment WHERE friend2_id='" . $this->id . "') OR player_id IN(SELECT friend2_id FROM fr_friend_assignment WHERE friend1_id='" . $this->id . "')");
-		$stmt->execute();
-		$i = 0;
-		foreach ($stmt as $row) {
-			$friends[$i] = new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
-			$i++;
-		}
-		if (isset($friends))
-			return $friends;
-		return [];
-	}
-
-	public function getFriendRequests(): array {
-		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("SELECT player_id, player_uuid, player_name FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "players WHERE player_id IN(SELECT requester_id FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "friend_request_assignment WHERE receiver_id='" . $this->id . "')");
-		$stmt->execute();
-		$i = 0;
-		foreach ($stmt as $row) {
-			$friends[$i] = new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
-			$i++;
-		}
-		if (isset($friends))
-			return $friends;
-		return [];
-	}
-
-	public function getSentFriendRequests(): array {
-		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("SELECT player_id, player_uuid, player_name FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "players WHERE player_id IN(SELECT receiver_id FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "friend_request_assignment WHERE requester_id='" . $this->id . "')");
-		$stmt->execute();
-		$i = 0;
-		foreach ($stmt as $row) {
-			$friends[$i] = new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
-			$i++;
-		}
-		if (isset($friends))
-			return $friends;
-		return [];
-	}
-
-	public function getSettingsWorth(int $pSettingsID): int {
-		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("SELECT settings_worth FROM " . PAFPlayerManager::getInstance()->getTablePrefix() . "settings WHERE player_id = '" . $this->id . "' AND settings_id = '" . $pSettingsID . "' LIMIT 1");
-		$stmt->execute();
-		if (isset($stmt[0]))
-			return $stmt[0]['settings_worth'];
-		return 0;
-	}
-
-	public function addFriend(PAFPlayer $player) {
-		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("INSERT INTO " . PAFPlayerManager::getInstance()->getTablePrefix() . "friend_assignment VALUES ('" . $this->id . "', '" . $player->getID() . "')");
-		$stmt->execute();
-	}
-
-	public function getID(): int {
+	public function getID() {
 		return $this->id;
 	}
 
-	public function sendFriendRequest(PAFPlayer $player) {
-		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("INSERT INTO " . PAFPlayerManager::getInstance()->getTablePrefix() . "friend_assignment VALUES ('" . $this->id . "', '" . $player->getID() . "')");
+	public function getFriends() {
+		$prefix = PAFPlayerManager::getInstance()->getTablePrefix();
+		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("SELECT player_id, player_uuid, player_name FROM {$prefix}players WHERE player_id IN(SELECT friend1_id FROM {$prefix}friend_assignment WHERE friend2_id='{$this->id}') OR player_id IN(SELECT friend2_id FROM fr_friend_assignment WHERE friend1_id='{$this->id}')");
 		$stmt->execute();
+		$i = 0;
+		foreach ($stmt as $row) {
+			$friends[$i] = new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
+			$i++;
+		}
+		if(isset($friends))
+			return $friends;
+	}
+	
+	public function getFriendRequests() {
+		$prefix = PAFPlayerManager::getInstance()->getTablePrefix();
+		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("SELECT player_id, player_uuid, player_name FROM {$prefix}players WHERE player_id IN(SELECT requester_id FROM {$prefix}friend_request_assignment WHERE receiver_id='{$this->id}')");
+		$stmt->execute();
+		$i = 0;
+		foreach ($stmt as $row) {
+			$friends[$i] = new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
+			$i++;
+		}
+		if(isset($friends))
+			return $friends;
+	}
+	
+	public function getSentFriendRequests() {
+		$prefix = PAFPlayerManager::getInstance()->getTablePrefix();
+		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("SELECT player_id, player_uuid, player_name FROM {$prefix}players WHERE player_id IN(SELECT receiver_id FROM {$prefix}friend_request_assignment WHERE requester_id='{$this->id}')");
+		$stmt->execute();
+		$i = 0;
+		foreach ($stmt as $row) {
+			$friends[$i] = new PAFPlayer($row['player_uuid'], $row['player_name'], $row['player_id']);
+			$i++;
+		}
+		if(isset($friends))
+			return $friends;
 	}
 
+	public function getSetting($setting) {
+		//try find setting in map
+		if ($this->settingMap[$setting]) {
+			$settingId = $this->settingMap[$setting];
+		} else {
+			return;
+		}
+
+		//get setting
+		$prefix = PAFPlayerManager::getInstance()->getTablePrefix();
+		$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("SELECT * FROM {$prefix}settings WHERE player_id = {$this->id} AND settings_id = {$settingId}");
+		$stmt->execute();
+
+		foreach ($stmt as $row) {
+			$worth = $row['settings_worth'];
+		}
+
+		//if the setting row exists, if not just return 0
+		if (isset($worth)) {
+			return $worth;
+		} else {
+			return 0;
+		}
+	}
+
+	public function setSetting($setting, $worth) {
+		if ($this->settingMap[$setting]) {
+			$settingId = $this->settingMap[$setting];
+		} else {
+			return false;
+		}
+
+		$prefix = PAFPlayerManager::getInstance()->getTablePrefix();
+		if ($this->getSetting($setting) !== 0) {
+			//already in db
+			if ($worth == 0) {
+				$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("DELETE FROM {$prefix}settings WHERE player_id = {$this->id} AND settings_id = {$settingId}");
+				$stmt->execute();
+				return true;
+			} else {
+				$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("UPDATE {$prefix}settings SET settings_worth = {$worth} WHERE player_id = {$this->id} AND settings_id = {$settingId}");
+				$stmt->execute();
+				return true;
+			}
+		} else {
+			//not in db
+			$stmt = PAFPlayerManager::getInstance()->getConnection()->prepare("INSERT INTO {$prefix}settings (player_id, settings_id, settings_worth) VALUES ({$this->id}, {$settingId}, {$worth})");
+			$stmt->execute();
+			return true;
+		}
+	}
 }
